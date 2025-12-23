f4f70e1dafd08b0b8ddac69b5fd7269d6d7914cf
LE TON ANH TUAN <tuannleeeee@gmail.com>
2025-12-24 02:24:20 +0700

    -0013-
    adding pornhub support for login type because i think it would be funny

--- model/Enums.go
+++ model/Enums.go
@@ -10,12 +10,13 @@ const (
 	CredTypeFacebook CredentialType = "facebook"
 	CredTypeGithub   CredentialType = "github"
 	CredTypeZalo     CredentialType = "zalo" // easy to add new ones here
+	CredTypePornhub  CredentialType = "pornhub"
 )
 
 // Optional: Helper to validate if a string is a valid enum
 func (ct CredentialType) IsValid() bool {
 	switch ct {
-	case CredTypePassword, CredTypeGoogle, CredTypeFacebook, CredTypeGithub, CredTypeZalo:
+	case CredTypePassword, CredTypeGoogle, CredTypeFacebook, CredTypeGithub, CredTypeZalo, CredTypePornhub:
 		return true
 	}
 	return false
